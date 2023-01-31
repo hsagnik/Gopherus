2b729a3d61764daa718dfbb2855b2d42d30c3051
Sagnik Haldar <52795867+hsagnik@users.noreply.github.com>
2023-01-31 08:40:44 +0530

    Update install.sh
    Updated code to execute it in Python3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python2 -m pip install argparse
-python2 -m pip install requests
+python3 -m pip install argparse
+python3 -m pip install requests
 chmod +x gopherus.py
 ln -sf $(pwd)/gopherus.py /usr/local/bin/gopherus
